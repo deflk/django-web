6651620943000a8dddcd725c9ce479a2c426de69
deflk <359692713@qq.com>
2018-05-22 10:03:05 +0800

    settings change

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a%dq_n^ror0^se%$kqghib+a$dxpf*@-sj79vd9)%o7qhnrtw-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deflk.pythonanywhere.com']
 
 
 # Application definition
