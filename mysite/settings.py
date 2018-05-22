a731c0dc41dc73ac0a9024835472a8c49c2d0494
deflk <359692713@qq.com>
2018-05-22 12:12:03 +0800

    some changes for deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a%dq_n^ror0^se%$kqghib+a$dxpf*@-sj79vd9)%o7qhnrtw-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deflk.pythonanywhere.com']
 
 
 # Application definition
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
