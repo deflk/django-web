1d76a1a41986e24709485f28950a1bc83590b5af
cahn <359692713@qq.com>
2018-09-21 13:08:25 +0800

    update ckeditor code highlight

--- comment/forms.py
+++ comment/forms.py
@@ -9,7 +9,7 @@ class CommentForm(forms.Form):
 	content_type = forms.CharField(widget=forms.HiddenInput)
 	object_id = forms.IntegerField(widget=forms.HiddenInput)
 	text = forms.CharField(
-							widget=CKEditorWidget(config_name='comment_ckeditor'),
+							widget=CKEditorWidget(config_name='default'),
 							error_messages={'required':'评论内容不能为空'})
 	reply_comment_id = forms.IntegerField(widget=forms.HiddenInput(attrs={'id':'reply_comment_id'}))
 
